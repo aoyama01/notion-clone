11ef0205e4b1af3d8a1dbeac202192b6d6ffb730
aoyama01 <139197283+aoyama01@users.noreply.github.com>
2025-03-13 17:04:06 +0900

    Add connection to MongoDB

--- server/index.js
+++ server/index.js
@@ -1,6 +1,16 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const PORT = 5000;
+require("dotenv").config();
+
+// MongoDBへの接続
+try {
+  mongoose.connect(process.env.MONGODB_URL);
+  console.log("MongoDBに接続しました．");
+} catch (error) {
+  console.log(error);
+}
 
 // http://localhost:5000/ ← ルートパス
 app.get("/", (req, res) => {
