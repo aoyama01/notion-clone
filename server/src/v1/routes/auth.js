471c7e7d7d94afd75bf3305d4715790a27573a15
aoyama01 <139197283+aoyama01@users.noreply.github.com>
2025-03-13 21:46:44 +0900

    Add JWT auth API

--- server/src/v1/routes/auth.js
+++ server/src/v1/routes/auth.js
@@ -4,6 +4,7 @@ const { body, validationResult } = require("express-validator");
 const User = require("../models/user");
 const validation = require("../handlers/validation");
 const userController = require("../controllers/user");
+const tokenHandler = require("../handlers/tokenHandler");
 
 // ユーザー新規登録API
 router.post(
@@ -41,5 +42,10 @@ router.post(
   validation.validate,
   userController.login
 );
+
+// JWT認証API
+router.post("/verify-token", tokenHandler.verifyToken, (req, res) => {
+  return res.status(200).json({ user: req.user });
+});
 
 module.exports = router;
