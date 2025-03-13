c1144af94f18c83347a7784162588d97b623522e
aoyama01 <139197283+aoyama01@users.noreply.github.com>
2025-03-13 19:39:07 +0900

    Add POST process

--- server/index.js
+++ server/index.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const CryptoJS = require("crypto-js");
+const JWT = require("jsonwebtoken");
+const User = require("./src/v1/models/user");
 const app = express();
 const PORT = 5000;
 require("dotenv").config();
+
+app.use(express.json()); // JSONの受け取り設定
 
 // MongoDBへの接続
 try {
@@ -11,6 +16,31 @@ try {
 } catch (error) {
   console.log(error);
 }
+
+// ユーザー新規登録API
+app.post(
+  "/register",
+  async (req, res) => {
+    // パスワードの受け取り
+    const password = req.body.password;
+    try {
+      // パスワードの暗号化
+      req.body.password = CryptoJS.AES.encrypt(
+        password,
+        process.env.SECRET_KEY
+      );
+      // ユーザーの新規作成
+      const user = await User.create(req.body);
+      // JWTの発行
+      const token = JWT.sign({ id: user._id }, process.env.TOKEN_SECRET_KEY, {
+        expiresIn: "24h",
+      });
+      return res.status(200).json({ user, token });
+    } catch (error) {
+      return res.status(500).json(error);
+    }
+  }
+);
 
 // http://localhost:5000/ ← ルートパス
 app.get("/", (req, res) => {
