671b8e36cc774cb4bc0b63d36fc3cc33b81a43eb
aoyama01 <139197283+aoyama01@users.noreply.github.com>
2025-03-22 18:43:36 +0900

    Install react-router-dom and mui

--- client/src/components/pages/Login.jsx
+++ client/src/components/pages/Login.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import AccessAlarmIcon from "@mui/icons-material/AccessAlarm";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div>
+      <AccessAlarmIcon />
+      Login
+    </div>
+  );
 };
 
 export default Login;
