4253f773c3effea1690930455fb787ab34338c29
aoyama01 <139197283+aoyama01@users.noreply.github.com>
2025-03-28 11:23:04 +0900

    Add sidebar

---
+++ client/src/components/common/Sidebar.jsx
@@ -0,0 +1,84 @@
+import {
+  Box,
+  Drawer,
+  IconButton,
+  List,
+  ListItemButton,
+  Typography,
+} from "@mui/material";
+import LogoutOutLinedIcon from "@mui/icons-material/LogoutOutlined";
+import AddBoxOutLinedIcon from "@mui/icons-material/AddBoxOutlined";
+import React from "react";
+import assets from "../../assets";
+
+const Sidebar = () => {
+  return (
+    <Drawer
+      container={window.document.body}
+      variant="permanent"
+      open={true}
+      sx={{ width: 240, height: "100vh" }}
+    >
+      <List
+        sx={{
+          width: 240,
+          height: "100vh",
+          backgroundColor: assets.colors.secondary,
+        }}
+      >
+        <ListItemButton>
+          <Box
+            sx={{
+              width: "100%",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+            }}
+          >
+            <Typography variant="body2" fontWeight="700">
+              shincode
+            </Typography>
+            <IconButton>
+              <LogoutOutLinedIcon />
+            </IconButton>
+          </Box>
+        </ListItemButton>
+        <Box sx={{ paddingTop: "10px" }}></Box>
+        <ListItemButton>
+          <Box
+            sx={{
+              width: "100%",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+            }}
+          >
+            <Typography variant="body2" fontWeight="700">
+              お気に入り
+            </Typography>
+          </Box>
+        </ListItemButton>
+        <Box sx={{ paddingTop: "10px" }}></Box>
+        <ListItemButton>
+          <Box
+            sx={{
+              width: "100%",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+            }}
+          >
+            <Typography variant="body2" fontWeight="700">
+              プライベート
+            </Typography>
+            <IconButton>
+              <AddBoxOutLinedIcon fontSize="small" />
+            </IconButton>
+          </Box>
+        </ListItemButton>
+      </List>
+    </Drawer>
+  );
+};
+
+export default Sidebar;
---
+++ client/src/components/layout/AppLayout.jsx
@@ -0,0 +1,34 @@
+import { Container, Box } from "@mui/material";
+import { React, useEffect } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
+import notionLogo from "../../assets/images/notion-logo.png";
+import authUtils from "../../utils/authUtils";
+import Sidebar from "../common/Sidebar";
+
+const AppLayout = () => {
+  const navigate = useNavigate();
+  useEffect(() => {
+    // JWTを持っているか確認
+    const checkAuth = async () => {
+      // 認証チェック
+      const user = await authUtils.isAuthenticated();
+      if (!user) {
+        navigate("/login"); // ログイン済みの場合はルートディレクトリに遷移
+      }
+    };
+    checkAuth();
+  }, [navigate]); // ページ遷移時に発火
+
+  return (
+    <div>
+      <Box sx={{ display: "flex" }}>
+        <Sidebar />
+        <Box sx={{ flexGrow: 1, p: 1, width: "max-content" }}>
+          <Outlet />
+        </Box>
+      </Box>
+    </div>
+  );
+};
+
+export default AppLayout;
