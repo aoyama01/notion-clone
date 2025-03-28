84bc0e57f889e05b8be53e8d8cfcba5e582b3e79
aoyama01 <139197283+aoyama01@users.noreply.github.com>
2025-03-28 11:14:32 +0900

    Add routing for app pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AuthLayout from "./components/layout/AuthLayout";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { CssBaseline } from "@mui/material";
+import AppLayout from "./components/layout/AppLayout";
+import Home from "./components/pages/Home";
 
 function App() {
   const theme = createTheme({ palette: { mode: "light" } });
@@ -17,6 +19,10 @@ function App() {
           <Route path="/" element={<AuthLayout />}>
             <Route path="login" element={<Login />} />
             <Route path="register" element={<Register />} />
+          </Route>
+          <Route path="/" element={<AppLayout />}>
+            <Route index element={<Home />} />
+            <Route path="memo" element={<Home />} />
           </Route>
         </Routes>
       </BrowserRouter>
